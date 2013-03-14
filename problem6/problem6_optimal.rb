5f9cb1981c10f2568fa62d61bb3672c10e8b3dee
Ryan Brubaker <ryan.brubaker@gmail.com>
2013-03-14 16:58:29 -0500

    Added optimal ruby solution

---
+++ problem6/problem6_optimal.rb
@@ -0,0 +1,7 @@
+up_to = 100
+
+square_of_sum = (up_to * (up_to + 1) / 2) ** 2
+sum_of_squares = ((2 * up_to) + 1) * (up_to + 1) * up_to / 6
+puts square_of_sum
+puts sum_of_squares
+puts square_of_sum - sum_of_squares
