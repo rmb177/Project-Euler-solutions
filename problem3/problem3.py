b887000eee6f9b66095323ebb87f258beb9c0ce8
Ryan Brubaker <ryan.brubaker@gmail.com>
2015-02-04 16:55:19 -0600

    Updated problem 3 solutions to fix Sieve of Eratosthenes algorithm.

--- problem3/problem3.py
+++ problem3/problem3.py
@@ -1,23 +1,26 @@
 # Find the largest prime factor of a composite number.
+# Using Sieve of Eratosthenes algorithm to find primes
 
 COMPOSITE = 600851475143
 SQRT_COMPOSITE = 775148     # Don't need to check past square root of composite
 
-flags = range(0, SQRT_COMPOSITE + 1)
+flags = [True] * (SQRT_COMPOSITE + 1)
 current_divisor = 2;
 largest_prime = -1
-
+num_iterations = 1
 while current_divisor < SQRT_COMPOSITE:
-   if flags[current_divisor]:
-      if 0 == COMPOSITE % current_divisor:
-         largest_prime = current_divisor
+   if 0 == (COMPOSITE % current_divisor):
+      largest_prime = current_divisor
       
-      # Don't have to check any other multiples of current divisor
-      index = current_divisor * 2
-      while index < SQRT_COMPOSITE:
-         flags[index] = False
-         index += current_divisor
+   index = current_divisor ** 2
+   while index < SQRT_COMPOSITE:
+      flags[index] = False
+      index += current_divisor
          
    current_divisor += 1
-
+   while not flags[current_divisor]:
+      current_divisor += 1
+   
+   num_iterations += 1
 print "\n", largest_prime, "\n"
+
--- problem3/problem3.rb
+++ problem3/problem3.rb
@@ -1,4 +1,5 @@
 # Find the largest prime factor of a composite number.
+# Using Sieve of Eratosthenes algorithm to find primes
 
 COMPOSITE = 600851475143
 SQRT_COMPOSITE = 775148     # Don't need to check past square root of composite
@@ -8,19 +9,21 @@ flags = Array.new(SQRT_COMPOSITE + 1, true)
 
 current_divisor = 2;
 largest_prime = -1
+num_iterations = 0
 
 while current_divisor < SQRT_COMPOSITE
-   if flags[current_divisor]
-      largest_prime = current_divisor if 0 == (COMPOSITE % current_divisor)
+   largest_prime = current_divisor if 0 == (COMPOSITE % current_divisor)
    
-      # Don't have to check any other multiples of current divisor
-      index = current_divisor * 2
-      while index < SQRT_COMPOSITE
-         flags[index] = false
-         index += current_divisor
-      end
+   index = current_divisor ** 2
+   while index < SQRT_COMPOSITE
+      flags[index] = false
+      index += current_divisor
    end
-   current_divisor += 1
+   
+   current_divisor += 1 
+   while not flags[current_divisor]
+      current_divisor += 1
+   end
 end
 
 print "\n", largest_prime, "\n\n"
